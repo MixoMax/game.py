8ad878613291cea744787539c190b9175526bf26
Linus Horn <linushorn@gmx.net>
2021-09-30 18:50:31 +0200

    Create game.py

---
+++ game.py
@@ -0,0 +1,35 @@
+import math
+import random
+import pygame
+import time
+
+if(0 == 0):
+    White = 255, 255, 255
+    Black = 0, 0, 0
+    Red = 255, 0, 0
+    Green = 0, 255, 0
+    Blue = 0, 0, 255
+    Yellow = 255, 255, 0
+    Pink = 255, 0, 255
+    Violet = 0, 255, 255
+    Orange = 255, 160, 0
+screenx = 500
+screeny = 500
+pygame.init()
+screen = pygame.display.set_mode((screenx, screeny))
+screen.fill((Black))
+
+while True:
+    pygame.draw.line(screen, (White), (0, 0), (500, 500))
+    pygame.display.update
+
+
+
+
+
+
+
+
+
+
+
